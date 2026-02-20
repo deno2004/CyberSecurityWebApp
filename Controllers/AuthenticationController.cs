86e04c91d6cefb9c53ba1d6c28c93ea07bd4706d
deni2004 <112943691+deno2004@users.noreply.github.com>
2026-02-20 14:54:39 +0100

    profile page

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -2,6 +2,8 @@
 using CyberSecurityWebApp.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,23 @@
-using System.Diagnostics;
+using CyberSecurityWebApp.Data;
 using CyberSecurityWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
 
 namespace CyberSecurityWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context, IWebHostEnvironment env)
         {
             _logger = logger;
+            _context = context;
+            _env = env;
         }
 
         public IActionResult Learn()
@@ -31,6 +38,21 @@ namespace CyberSecurityWebApp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Profile()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null)
+                return RedirectToAction("Login", "Authentication");
+
+            var user = await _context.Users.FindAsync(int.Parse(userId));
+            if (user == null)
+                return NotFound();
+
+            return View(user);
         }
     }
 }
