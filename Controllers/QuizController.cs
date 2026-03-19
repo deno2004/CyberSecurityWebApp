eaf2d66c50d65a247fbdabb6b90e1f70ff987c89
deni2004 <112943691+deno2004@users.noreply.github.com>
2026-03-19 22:55:57 +0100

    share link

--- Controllers/QuizController.cs
+++ Controllers/QuizController.cs
@@ -152,189 +152,7 @@ namespace CyberSecurityWebApp.Controllers
             return View(quiz);
         }
 
-        /*public async Task<IActionResult> Index()
-        {
-            var modules = await _context.QuizModules
-                .Include(m => m.Questions)
-                .ToListAsync();
-            return View(modules);
-        }
-
-        public async Task<IActionResult> Start(int id)
-        {
-            var module = await _context.QuizModules
-                .Include(m => m.Questions)
-                    .ThenInclude(q => q.Answers)
-                .FirstOrDefaultAsync(m => m.Id == id);
-
-            if (module == null)
-                return NotFound();
-
-            return View(module);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateSession(int moduleId, string userName)
-        {
-            if (string.IsNullOrWhiteSpace(userName))
-            {
-                TempData["Error"] = "Prosimo, vnesite vaše ime.";
-                return RedirectToAction("Start", new { id = moduleId });
-            }
-
-            var sessionCode = GenerateSessionCode();
-
-            var module = await _context.QuizModules
-                .Include(m => m.Questions)
-                .FirstOrDefaultAsync(m => m.Id == moduleId);
-
-            if (module == null)
-                return NotFound();
-
-            var session = new QuizSession
-            {
-                SessionCode = sessionCode,
-                UserName = userName,
-                QuizModuleId = moduleId,
-                CreatedAt = DateTime.Now,
-                TotalQuestions = module.Questions.Count
-            };
-
-            _context.QuizSessions.Add(session);
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction("TakeQuiz", new { code = sessionCode });
-        }
-
-        public async Task<IActionResult> TakeQuiz(string code)
-        {
-            var session = await _context.QuizSessions
-                .Include(s => s.UserAnswers)
-                .FirstOrDefaultAsync(s => s.SessionCode == code);
-
-            if (session == null)
-                return NotFound();
-
-            if (session.CompletedAt != null)
-                return RedirectToAction("Results", new { code = code });
-
-            var module = await _context.QuizModules
-                .Include(m => m.Questions)
-                    .ThenInclude(q => q.Answers)
-                .FirstOrDefaultAsync(m => m.Id == session.QuizModuleId);
-
-            if (module == null)
-                return NotFound();
-
-            ViewBag.Session = session;
-            ViewBag.CurrentQuestion = session.UserAnswers.Count + 1;
-
-            return View(module);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> SubmitAnswer(string code, int questionId, int answerId)
-        {
-            var session = await _context.QuizSessions
-                .Include(s => s.UserAnswers)
-                .FirstOrDefaultAsync(s => s.SessionCode == code);
-
-            if (session == null)
-                return NotFound();
-
-            var answer = await _context.Answers
-                .FirstOrDefaultAsync(a => a.Id == answerId);
-
-            if (answer == null)
-                return BadRequest();
-
-            var userAnswer = new UserAnswer
-            {
-                QuizSessionId = session.Id,
-                QuestionId = questionId,
-                SelectedAnswerId = answerId,
-                IsCorrect = answer.IsCorrect
-            };
-
-            _context.UserAnswers.Add(userAnswer);
-
-            if (answer.IsCorrect)
-                session.Score++;
-
-            // Check if quiz is completed
-            var totalQuestions = await _context.Questions
-                .Where(q => q.QuizModuleId == session.QuizModuleId)
-                .CountAsync();
-
-            if (session.UserAnswers.Count + 1 >= totalQuestions)
-            {
-                session.CompletedAt = DateTime.Now;
-            }
-
-            await _context.SaveChangesAsync();
-
-            if (session.CompletedAt != null)
-                return RedirectToAction("Results", new { code = code });
-
-            return RedirectToAction("TakeQuiz", new { code = code });
-        }
-
-        public async Task<IActionResult> Results(string code)
-        {
-            var session = await _context.QuizSessions
-                .Include(s => s.UserAnswers)
-                .FirstOrDefaultAsync(s => s.SessionCode == code);
-
-            if (session == null)
-                return NotFound();
-
-            var questions = await _context.Questions
-                .Include(q => q.Answers)
-                .Where(q => q.QuizModuleId == session.QuizModuleId)
-                .ToListAsync();
-
-            var results = new List<QuestionResult>();
-
-            foreach (var question in questions)
-            {
-                var userAnswer = session.UserAnswers
-                    .FirstOrDefault(ua => ua.QuestionId == question.Id);
-
-                if (userAnswer != null)
-                {
-                    var selectedAnswer = question.Answers
-                        .First(a => a.Id == userAnswer.SelectedAnswerId);
-                    var correctAnswer = question.Answers
-                        .First(a => a.IsCorrect);
-
-                    results.Add(new QuestionResult
-                    {
-                        Question = question,
-                        SelectedAnswer = selectedAnswer,
-                        CorrectAnswer = correctAnswer,
-                        IsCorrect = userAnswer.IsCorrect
-                    });
-                }
-            }
-
-            var percentage = (double)session.Score / session.TotalQuestions * 100;
-
-            var viewModel = new QuizResultViewModel
-            {
-                Session = session,
-                Results = results,
-                FeedbackMessage = GetFeedbackMessage(percentage),
-                FeedbackClass = GetFeedbackClass(percentage)
-            };
-
-            return View(viewModel);
-        }
-
-        public IActionResult Share(int id)
-        {
-            ViewBag.ModuleId = id;
-            return View();
-        }
+        
 
         [HttpPost]
         public async Task<IActionResult> GenerateLink(int moduleId, string userName)
@@ -347,23 +165,22 @@ namespace CyberSecurityWebApp.Controllers
 
             var sessionCode = GenerateSessionCode();
 
-            var module = await _context.QuizModules
+            var module = await _context.Quizzes
                 .Include(m => m.Questions)
-                .FirstOrDefaultAsync(m => m.Id == moduleId);
+                .FirstOrDefaultAsync(m => m.QuizId == moduleId);
 
             if (module == null)
                 return NotFound();
 
-            var session = new QuizSession
+            var session = new Models.Quiz
             {
-                SessionCode = sessionCode,
-                UserName = userName,
-                QuizModuleId = moduleId,
-                CreatedAt = DateTime.Now,
-                TotalQuestions = module.Questions.Count
+                Title = $"{module.Title} - Seja za {userName}",
+                Description = $"Seja kviza za uporabnika {userName}",
+                IconClass = module.IconClass,
+                PassThreshold = module.PassThreshold
             };
 
-            _context.QuizSessions.Add(session);
+            _context.Quizzes.Add(session);
             await _context.SaveChangesAsync();
 
             var shareUrl = Url.Action("TakeQuiz", "Quiz", new { code = sessionCode }, Request.Scheme);
@@ -402,6 +219,6 @@ namespace CyberSecurityWebApp.Controllers
                 >= 50 => "average",
                 _ => "needs-improvement"
             };
-        }*/
+        }
     }
 }
